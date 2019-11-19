e72c5dddfa7c75d7df15f3f3b41ca0031312636c
Michael Variola <michael.variola@gmail.com>
2019-11-19 16:26:55 +0100

    Added robo constructor

--- src/lagerung/Robo.h
+++ src/lagerung/Robo.h
@@ -5,9 +5,23 @@
 #ifndef SRC_ROBO_H
 #define SRC_ROBO_H
 
+#include "Shelf.h"
 
 class Robo {
+private:
+    Shelf left_;
+    Shelf right_;
+public:
+    const float maxHorizontalVel_;
+    const float maxVerticalVel_;
+    const float horizontalVel_;
+    const float verticalVel_;
+    const float timeForDeploy_;
 
+    Robo(const Shelf& left, const Shelf& right, float horizontalVel, float max_horizontal_vel, float verticalVel,
+            float max_veritcal_vel, float timeForDeploy = 5) : left_(left), right_(right),
+            maxHorizontalVel_(max_horizontal_vel), maxVerticalVel_(max_veritcal_vel), horizontalVel_(horizontalVel),
+            verticalVel_(verticalVel), timeForDeploy_(timeForDeploy) {}
 };
 
 
