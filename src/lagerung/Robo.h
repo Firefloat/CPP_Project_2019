26d919b4ecc880a46d074a789f67e231b1865817
Michael Variola <michael.variola@gmail.com>
2019-11-19 17:17:51 +0100

    Set and get container methods

--- src/lagerung/Robo.h
+++ src/lagerung/Robo.h
@@ -5,13 +5,18 @@
 #ifndef SRC_ROBO_H
 #define SRC_ROBO_H
 
+#include <queue>
 #include "Shelf.h"
+#include "Container.h"
 
 class Robo {
 private:
     Shelf left_;
     Shelf right_;
+
 public:
+    std::queue<Container> leftQueue_{};
+    std::queue<Container> rightQueue_{};
     const float maxHorizontalVel_;
     const float maxVerticalVel_;
     const float horizontalVel_;
@@ -21,7 +26,10 @@ public:
     Robo(const Shelf& left, const Shelf& right, float horizontalVel, float max_horizontal_vel, float verticalVel,
             float max_veritcal_vel, float timeForDeploy = 5) : left_(left), right_(right),
             maxHorizontalVel_(max_horizontal_vel), maxVerticalVel_(max_veritcal_vel), horizontalVel_(horizontalVel),
-            verticalVel_(verticalVel), timeForDeploy_(timeForDeploy) {}
+            verticalVel_(verticalVel), timeForDeploy_(timeForDeploy) { }
+
+    Container GetContainer(int prio);
+    void SetContainer(int prio, Container container);
 };
 
 
