5ec0b8982880af06756d4e947ebc6367016cd7cb
Michael Variola <michael.variola@gmail.com>
2019-12-29 17:30:38 +0100

    Adjusted white space

--- src/lagerung/Robo.h
+++ src/lagerung/Robo.h
@@ -26,37 +26,20 @@ public:
     float horizontalAcceleration_;
     float horizontalVelocity_;
     float timeStoreRestore_;
-
     Coordinates coordinates_;
     Coordinates inputCoordinates_;
     Coordinates outputCoordinates_;
     Queue queue_;
     Shelf leftShelf_;
     Shelf rightShelf_;
-
     std::queue<std::pair<FunctionType , Coordinates>> actionQueue_;
-
     // TODO: Actionqueue
-
     Robo(Coordinates coordinates, Coordinates inputCoordinates, Coordinates outputCoordinates);
-
     double GetMovingTime(Coordinates targetCoordinates);
-
     void AddToActionQueue(FunctionType functionType, Coordinates coordinates);
     void Do();
     bool Store(Coordinates coordinates);
     bool Remove(Coordinates coordinates);
-
-
-
-
-
-
-
-
-
-
-
 };
 
 
