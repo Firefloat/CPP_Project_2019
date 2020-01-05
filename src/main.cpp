d1cf504a299180e746eb2e3e3f837a006ff444bf
Michael Variola <michael.variola@gmail.com>
2020-01-05 12:42:23 +0100

    User Interaction prototype

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,18 @@ std::uniform_int_distribution<> quantityDist{0, 100};
 
 Container GenerateContainer(){
 
-    Container tempContainer{Size{0.6, 0.6, 0.6},
-                            Article{ArticleType::muttern, static_cast<Priority>(intDist(eng)),
-                                    static_cast<unsigned int>(quantityDist(eng))}};
+//    Container tempContainer{Size{0.6, 0.6, 0.6},
+//                            Article{ArticleType::muttern, static_cast<Priority>(intDist(eng)),
+//                                    static_cast<unsigned int>(quantityDist(eng))}};
 
+    Container tempContainer{Size{0.6, 0.6, 0.6},
+                            Article{ArticleType::muttern, Priority::medium,
+                                    static_cast<unsigned int>(quantityDist(eng))}};
     return tempContainer;
 }
 
-int main() {
-    Loaderton::Instance().Setup();
-
-    // create storage manager
-    StorageManager storageManager{};
+void UserInteraction(StorageManager& storageManager){
     bool notFinished = true;
-
     while(notFinished){
 
 
@@ -37,6 +35,48 @@ int main() {
         std::cout << "Press any key to continue \n";
         std::getchar();
     }
+}
+
+void Simulation(StorageManager& storageManager){
+    bool notFinished = true;
+    while(notFinished){
+
+
+        // initialize test container
+        Container testContainer = GenerateContainer();
+
+        storageManager.AddToQueue(testContainer);
+
+        std::cout << "Press any key to continue \n";
+        std::getchar();
+    }
+}
+
+int main() {
+    Loaderton::Instance().Setup();
+
+    // create storage manager
+    StorageManager storageManager{};
+
+    std::cout << "For simulation press 's'\n\n";
+    std::cout << "For interaction press 'i'\n\n";
+
+//    int c = std::getchar();
+
+    int c = 's';
+
+    if (c == 's' || c == 'S'){
+        Simulation(storageManager);
+    }
+    if (c == 'i' || c == 'I'){
+        UserInteraction(storageManager);
+    }
+    else{
+        std::cout << "Wrong character abort!!!!!!";
+        exit(13);
+    }
+
+
     Loaderton::Instance().Save();
     return 0;
 }
