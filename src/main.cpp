be9018885864ddf390e1b2e44d458db9527368a4
Michael Variola <michael.variola@gmail.com>
2020-01-05 12:10:27 +0100

    Wait for enter

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,14 @@
 unsigned int rd{42};
 std::mt19937 eng{rd};
 std::uniform_int_distribution<> intDist{0, 3};
+std::uniform_int_distribution<> quantityDist{0, 100};
 // --------------------------------------------------------
 
 Container GenerateContainer(){
 
     Container tempContainer{Size{0.6, 0.6, 0.6},
-                            Article{ArticleType::muttern, static_cast<Priority>(intDist(eng)), 6}};
+                            Article{ArticleType::muttern, static_cast<Priority>(intDist(eng)),
+                                    static_cast<unsigned int>(quantityDist(eng))}};
 
     return tempContainer;
 }
@@ -25,12 +27,15 @@ int main() {
     bool notFinished = true;
 
     while(notFinished){
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+
 
         // initialize test container
         Container testContainer = GenerateContainer();
 
         storageManager.AddToQueue(testContainer);
+
+        std::cout << "Press any key to continue \n";
+        std::getchar();
     }
     Loaderton::Instance().Save();
     return 0;
